713c845df2144e13c3447d04499a276c1d66f2d9
tonysaur05 <tonysaur@gmail.com>
2014-05-20 20:11:41 -0500

    Initial Commit

---
+++ Stocks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Stocks
+//
+//  Created by Tony Lam on 5/20/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
